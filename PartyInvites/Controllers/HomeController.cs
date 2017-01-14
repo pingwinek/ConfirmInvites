d0c0bdb86c24d4183d5cdf12c65ac837339e32ae
pingwinek <gornik.lukasz@gmail.com>
2017-01-14 19:15:18 +0100

    Create repository

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 18 ? "Dzień dobry" : "Dobry wieczór";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podaj swoje imię i nazwisko")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj swój email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Adres email jest niepoprawny")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj swój nr telefonu")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę określ czy weźmiesz udział")]
+        public bool? WillAttend { get; set; }
+    }
+}
